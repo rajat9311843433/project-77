36808777c06f41157b010374e93fc46e6d1ca67c
Rajat Sharma <72334413+rajat9311843433@users.noreply.github.com>
2021-07-11 15:03:49 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,59 @@
+console.log("Hi, this is console screen");
+var fname = "Rajat ";
+console.log(fname);
+var lname = "Sharma";
+console.log(lname);
+var fullname = fname.concat(lname);
+console.log(fullname);
+
+var length_fullname = fullname.length;
+
+console.log(length_fullname);
+
+var wrongname = ("I am jon rathor");
+
+console.log(wrongname);
+
+var correctname = wrongname.replace("rathor", "Sharma");
+
+console.log("correct name is :")
+
+console.log(correctname);
+
+var uppercase = "RAJAT";
+
+console.log(uppercase);
+
+var lowercase = uppercase.toLowerCase();
+
+console.log(lowercase);
+
+var uppercase2 = lowercase.toUpperCase();
+
+console.log(uppercase2);
+
+var math = Math.sqrt(16);
+
+console.log(math);
+
+var math2 = Math.sqrt(100000000045452734737);
+
+console.log(math2);
+
+var max = Math.max(10, 124393457, 3427824174782479, 394348954895898934, 230, 4327894842);
+
+console.log(max);
+
+var min = Math.min(1.0000000000000000000000000000000000, 1.3000000000000000000000000000000, 1.3233333333333333333333333333333333333333)
+
+console.log(min)
+
+var power = Math.pow(5, 2);
+
+console.log(power);
+
+var num1 = 7;
+
+var num2 = 7;
+
+console.log(num1 + num2);
